0befabf9c0e531e0f9c2e68af2ebc0d6a9b4c068
rusty <lozano.jacob@gmail.com>
2011-11-22 00:41:05 -0500

    firs java file

---
+++ src/org/gooiuce/Main.java
@@ -0,0 +1,9 @@
+package org.gooiuce;
+
+public class Main {
+	
+	public static void main(String [] args) {
+		System.out.println("Looking for the quote about large systems starting out as small systems.");
+	}
+
+}
